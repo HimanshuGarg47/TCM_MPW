8b1c437d986c8b37bb1e3d217b7894cf5c8a7bca
Himanshuga08 <himan.gargcoder@gmail.com>
2022-05-17 22:36:10 +0530

    hoem page process

--- mysite/Home/views.py
+++ mysite/Home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 # Create your views here.
 def index(request):
-    return HttpResponse("hello");
+    return render(request,'Home/index.html');
+
+
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,4 +20,7 @@ urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('', include('Home.urls')),
+    path('blogs/' , include('Blog.urls')),
+    path('User/', include('Authentication.urls')),
+    
 ]
